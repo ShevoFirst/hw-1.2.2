339ba792891510c4e937d51c304c1cf793f5543d
ShevoFirst <play.job@yandex.ru>
2022-10-17 17:02:52 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        //задание 4
+        var friend =19;
+        System.out.println(friend);
+        friend*=2;
+        System.out.println(friend);
+        friend/=7;
+        System.out.println(friend);
+        //задание 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog*=10;
+        System.out.println(frog);
+        frog/=3.5;
+        System.out.println(frog);
+        frog +=4;
+        System.out.println(frog);
+        
+
+    }
+}
